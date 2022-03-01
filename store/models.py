48c5cf294ea8c01fe1378bca24b061f0fc05053a
antsrp <72520534+antsrp@users.noreply.github.com>
2022-03-01 20:29:58 +0300

    Add files via upload

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
 import datetime
+
+from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import models
 
@@ -11,7 +13,26 @@ class Product(models.Model):
         return self.name
 
 
-class User(models.Model):
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    date = models.DateTimeField('date created')
+
+    def __str__(self):
+        return "%s %s %s" % (self.id, self.user.username, self.date)
+
+
+class Purchase(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, default=None)
+    # product = models.ManyToManyField(Product)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, default=None)
+    quantity = models.IntegerField(default=None)
+
+    def __str__(self):
+        return "%s %s %s" % (self.order, self.product, self.quantity)
+
+
+
+"""class User(models.Model):
     login = models.CharField(max_length=20)
     password = models.CharField(max_length=256)
     create_date = models.DateTimeField('date created')
@@ -20,19 +41,26 @@ class User(models.Model):
         return self.login
 
     def created_recently(self):
-        return self.create_date >= timezone.now() - datetime.delta(days=7)
+        return self.create_date >= timezone.now() - datetime.delta(days=7)"""
 
-
+"""
 class Purchase(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=None)
+    # product = models.ManyToManyField(Product)
+    quantity = models.IntegerField(blank=False, null=False, default=None)
 
     def __str__(self):
-        return self.user
+        return "%s, %s, %s" % (self.user.username, self.product, self.quantity)
+        # return self.user.name
 
 
 class Order(models.Model):
-    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    # purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    purchase = models.ManyToManyField(Purchase)
+    date = models.DateTimeField('date created', default=None)
 
     def __str__(self):
-        return self.purchase
+        return "%s, %s" % (self.purchase, self.date)
+"""
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 from .models import Product
@@ -9,6 +9,7 @@ def index(request):
     product_list = Product.objects.all()
     context = {
         'product_list': product_list,
+        'auth': request.user.is_authenticated
     }
     return render(request, 'products/index.html', context)
     # return HttpResponse("Hello, world. You're at the polls index.")
@@ -16,3 +17,9 @@ def index(request):
 
 def purchase(request, product_id):
     return HttpResponse("You're purchasing product %s." % product_id)
+
+
+def create_order(request):
+    kek = 0
+    return HttpResponse("You're just ordered something!" )
+    #return redirect('dashboard')
