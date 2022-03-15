905a501fcce78dcc875af822a054f0a3a679d014
antsrp <72520534+antsrp@users.noreply.github.com>
2022-03-15 17:48:40 +0300

    update store

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
 import datetime
+
+from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import models
 
@@ -11,28 +13,20 @@ class Product(models.Model):
         return self.name
 
 
-class User(models.Model):
-    login = models.CharField(max_length=20)
-    password = models.CharField(max_length=256)
-    create_date = models.DateTimeField('date created')
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    date = models.DateTimeField('date created')
 
     def __str__(self):
-        return self.login
-
-    def created_recently(self):
-        return self.create_date >= timezone.now() - datetime.delta(days=7)
+        return "%s %s %s" % (self.id, self.user.username, self.date)
 
 
 class Purchase(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, default=None)
+    # product = models.ManyToManyField(Product)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, default=None)
+    quantity = models.IntegerField(default=None)
 
     def __str__(self):
-        return self.user
+        return "%s %s %s" % (self.order, self.product, self.quantity)
 
-
-class Order(models.Model):
-    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.purchase
